96de189744c779ab7b2f13ebcbcb10dc8f6d0506
vitormmh <74314572+vitormmh@users.noreply.github.com>
2020-11-25 19:27:35 +0000

    Primeiro commit

---
+++ AcessoDados/LivroContexto.cs
@@ -0,0 +1,27 @@
+﻿using DemoCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DemoCRUD.AcessoDados
+{
+    public class LivroContexto : DbContext
+    {
+
+            public DbSet<Genero> Generos { get; set; }
+            public DbSet<Livro> Livros { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Properties<string>().Configure(c => c.HasMaxLength(100));
+        }
+            
+        
+    }
+}
